2b619877dd5d21da210b2d0d137642ee4a32b4df
theo43 <27286826+theo43@users.noreply.github.com>
2024-01-30 14:40:49 +0100

    feat(github_aws): update pipeline, entry_point and training Dockerfile

--- experiment/train/entry_point_train.py
+++ experiment/train/entry_point_train.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
-import numpy as np
+#import numpy as np
 import os
-
+import argparse
+import mlflow
 
 
 SEQ_LENGTH = 100
@@ -23,88 +24,100 @@ def split_input_target(sequence):
 
 
 class ShakespeareModel(tf.keras.Model):
-  def __init__(self, vocab_size, embedding_dim, rnn_units):
-    super().__init__(self)
-    self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-    self.gru = tf.keras.layers.GRU(
-       rnn_units,
-       return_sequences=True,
-       return_state=True
-    )
-    self.dense = tf.keras.layers.Dense(vocab_size)
+    def __init__(self, vocab_size, embedding_dim, rnn_units):
+        super().__init__(self)
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(
+          rnn_units,
+          return_sequences=True,
+          return_state=True
+        )
+        self.dense = tf.keras.layers.Dense(vocab_size)
 
-  def call(self, inputs, states=None, return_state=False, training=False):
-    x = inputs
-    x = self.embedding(x, training=training)
-    if states is None:
-      states = self.gru.get_initial_state(x)
-    x, states = self.gru(x, initial_state=states, training=training)
-    x = self.dense(x, training=training)
+    def call(self, inputs, states=None, return_state=False, training=False):
+        x = inputs
+        x = self.embedding(x, training=training)
+        if states is None:
+          states = self.gru.get_initial_state(x)
+        x, states = self.gru(x, initial_state=states, training=training)
+        x = self.dense(x, training=training)
 
-    if return_state:
-      return x, states
-    else:
-      return x
+        if return_state:
+          return x, states
+        else:
+          return x
 
 
 if __name__ == '__main__':
-    
-    train_data_path = os.environ['SM_CHANNEL_TRAINING']
+    with mlflow.start_run():
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+          '--train-data', type=str,
+          default=os.environ['SM_CHANNEL_TRAINING']
+        )
+        args = parser.parse_args()
 
-    text = open(train_data_path, 'rb').read().decode(encoding='utf-8')
-    vocab = sorted(set(text))
+        text = open(args.train_data, 'rb').read().decode(encoding='utf-8')
+        vocab = sorted(set(text))
 
-    # get ids from chars and reversed
-    ids_from_chars = tf.keras.layers.StringLookup(
-        vocabulary=list(vocab), mask_token=None
-    )
-    chars_from_ids = tf.keras.layers.StringLookup(
-        vocabulary=ids_from_chars.get_vocabulary(),
-        invert=True, mask_token=None
-    )
+        # get ids from chars and reversed
+        ids_from_chars = tf.keras.layers.StringLookup(
+            vocabulary=list(vocab), mask_token=None
+        )
+        chars_from_ids = tf.keras.layers.StringLookup(
+            vocabulary=ids_from_chars.get_vocabulary(),
+            invert=True, mask_token=None
+        )
 
-    # data preparation into sequences
-    shakespeare_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+        # data preparation into sequences
+        shakespeare_ids = ids_from_chars(
+            tf.strings.unicode_split(text, 'UTF-8')
+        )
 
-    ids_dataset = tf.data.Dataset.from_tensor_slices(shakespeare_ids)
+        ids_dataset = tf.data.Dataset.from_tensor_slices(shakespeare_ids)
 
-    sequences = ids_dataset.batch(SEQ_LENGTH+1, drop_remainder=True)
+        sequences = ids_dataset.batch(SEQ_LENGTH+1, drop_remainder=True)
 
-    dataset = sequences.map(split_input_target)
+        dataset = sequences.map(split_input_target)
 
-    # create training batches
-    dataset = dataset.shuffle(BUFFER_SIZE)\
-        .batch(BATCH_SIZE, drop_remainder=True)\
-        .prefetch(tf.data.experimental.AUTOTUNE)
-    
-    # Length of the vocabulary in StringLookup Layer
-    vocab_size = len(ids_from_chars.get_vocabulary())
+        # create training batches
+        dataset = dataset.shuffle(BUFFER_SIZE)\
+            .batch(BATCH_SIZE, drop_remainder=True)\
+            .prefetch(tf.data.experimental.AUTOTUNE)
+        
+        # Length of the vocabulary in StringLookup Layer
+        vocab_size = len(ids_from_chars.get_vocabulary())
 
-    # The embedding dimension
-    embedding_dim = 256
+        # The embedding dimension
+        embedding_dim = 256
 
-    # Number of RNN units
-    rnn_units = 1024
+        # Number of RNN units
+        rnn_units = 1024
 
-    model = ShakespeareModel(
-       vocab_size=vocab_size,
-       embedding_dim=embedding_dim,
-       rnn_units=rnn_units
-    )
+        model = ShakespeareModel(
+          vocab_size=vocab_size,
+          embedding_dim=embedding_dim,
+          rnn_units=rnn_units
+        )
 
-    loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
-    model.compile(optimizer='adam', loss=loss)
+        loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+        model.compile(optimizer='adam', loss=loss)
 
-    # Directory where the checkpoints will be saved
-    checkpoint_dir = './training_checkpoints'
-    # Name of the checkpoint files
-    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+        # Directory where the checkpoints will be saved
+        checkpoint_dir = './training_checkpoints'
+        # Name of the checkpoint files
+        checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_prefix,
-        save_weights_only=True)
-    
-    EPOCHS = 1
-    history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+        checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=checkpoint_prefix,
+            save_weights_only=True)
+        
+        EPOCHS = 1
+        history = model.fit(
+            dataset,
+            epochs=EPOCHS,
+            callbacks=[checkpoint_callback]
+        )
 
-    
+        # Log the model in mlflow
+        mlflow.tensorflow.log_model(model, 'shakespeare_tf_model')
--- experiment/train/pipeline_train.py
+++ experiment/train/pipeline_train.py
@@ -1,7 +1,7 @@
-from sagemaker.workflow.pipeline_context import LocalPipelineSession
-from sagemaker.tensorflow import TensorFlow
-import sagemaker
-from sagemaker.workflow.steps import TrainingStep, TrainingInput
+from sagemaker.workflow.pipeline_context import (
+    LocalPipelineSession, PipelineSession
+)
+from sagemaker.workflow.steps import TrainingStep
 from sagemaker.workflow.pipeline import Pipeline
 from sagemaker.estimator import Estimator
 import argparse
@@ -16,38 +16,47 @@ if __name__ == '__main__':
         '--s3-bucket-name', type=str, help='AWS S3 bucket name'
     )
     parser.add_argument(
-        '--role', type=str, help='AWS role'
+        '--aws-role', type=str, help='AWS role'
     )
     parser.add_argument(
         '--image-uri', type=str, help='Training image URI'
     )
+    parser.add_argument(
+        '--local', type=bool, default=False,
+        help='Local mode execution'
+    )
 
     args = parser.parse_args()
     s3_bucket_name = args.s3_bucket_name
-    role = args.role
+    role = args.aws_role
     image_uri = args.image_uri
+    local = args.local
 
-    session = LocalPipelineSession()
-    session.config = {'local': {'local_code': True}}
-    instance_count = 1
-    instance_type = 'local'
+    if local:
+        session = LocalPipelineSession()
+        session.config = {'local': {'local_code': True}}
+        instance_count = 1
+        instance_type = 'local'
+    else:
+        session = PipelineSession()
+        instance_count = 1
+        instance_type = 'ml.m5.xlarge'
 
     estimator = Estimator(
        image_uri=image_uri,
        role=role,
        instance_type=instance_type,
        instance_count=instance_count,
-       output_path='',
+       entry_point='entry_point_train.py'
     )
-    estimator.set_hyperparameters({'shm_size': '1G'})
 
     s3_train_data = f's3://{s3_bucket_name}/datasets/shakespeare/shakespeare.txt'
 
     step = TrainingStep(
         name="Shakespeare training step",
-        step_args=estimator.fit(
-            inputs=TrainingInput(s3_data=s3_train_data)
-            )
+        step_args=estimator.fit({
+            'training': s3_train_data
+        })
     )
 
     pipeline = Pipeline(
