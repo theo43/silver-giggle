26a445859665961fe435814ec46bead6a10c4b53
theo43 <27286826+theo43@users.noreply.github.com>
2024-02-15 10:42:00 +0100

    feat(validation): retry with custom image

--- experiment/train/pipeline_train.py
+++ experiment/train/pipeline_train.py
@@ -12,7 +12,7 @@ from sagemaker.workflow.parameters import (
 from sagemaker.processing import (
     ProcessingInput, ProcessingOutput, ScriptProcessor
 )
-# from sagemaker.estimator import Estimator
+from sagemaker.estimator import Estimator
 from sagemaker.tensorflow import TensorFlow
 from sagemaker.sklearn.processing import SKLearnProcessor
 from sagemaker.inputs import TrainingInput
@@ -111,29 +111,29 @@ if __name__ == '__main__':
 
     # Training step
     output_path = f's3://{s3_bucket_name}/models/estimator-models'
-    # estimator = Estimator(
-    #    image_uri=image_uri,
-    #    role=role,
-    #    instance_type=instance_type,
-    #    instance_count=instance_count,
-    #    source_dir='src',
-    #    entry_point='entrypoint_train.py',
-    #    output_path=output_path,
-    #    training_repository_access_mode='Vpc',
-    #    subnets=[
-    #        args.subnet_id1, args.subnet_id2, args.subnet_id3
-    #    ],
-    #    security_group_ids=[args.security_group_id]
-    # )
-    estimator = TensorFlow(
-        py_version='py310',
-        framework_version='2.13',
-        role=role,
-        instance_type=instance_type,
-        instance_count=instance_count,
-        source_dir='src',
-        entry_point='entrypoint_train.py',
-        output_path=output_path
+    estimator = Estimator(
+       image_uri=image_uri,
+       role=role,
+       instance_type=instance_type,
+       instance_count=instance_count,
+       source_dir='src',
+       entry_point='entrypoint_train.py',
+       output_path=output_path,
+       training_repository_access_mode='Vpc',
+       subnets=[
+           args.subnet_id1, args.subnet_id2, args.subnet_id3
+       ],
+       security_group_ids=[args.security_group_id]
+    )
+    # estimator = TensorFlow(
+    #     py_version='py310',
+    #     framework_version='2.13',
+    #     role=role,
+    #     instance_type=instance_type,
+    #     instance_count=instance_count,
+    #     source_dir='src',
+    #     entry_point='entrypoint_train.py',
+    #     output_path=output_path
     )
     train_input = TrainingInput(
         s3_data=step_data_process.properties.ProcessingOutputConfig.Outputs[
