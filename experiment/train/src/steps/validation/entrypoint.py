5916a9ff616d71f822f4f2b4f90bc5c3410b6099
theo43 <27286826+theo43@users.noreply.github.com>
2024-03-04 16:20:15 +0100

    feat(trznslation): update validation entrypoint

--- experiment/train/src/steps/validation/entrypoint.py
+++ experiment/train/src/steps/validation/entrypoint.py
@@ -2,15 +2,56 @@ import json
 import torch
 import torch.nn as nn
 from pathlib import Path
-import argparse
 from datasets import load_dataset
+from torchmetrics.text import BLEUScore
 from tokenizers import Tokenizer
 from translation.config import get_config
 from translation.model import get_model
+from translation.dataset import causal_mask
+
+
+def greedy_decode(
+    model, source, source_mask, tokenizer_src, tokenizer_tgt,
+    max_len, device
+):
+    sos_id = tokenizer_tgt.token_to_id('[SOS]')
+    eos_id = tokenizer_tgt.token_to_id('[EOS]')
+
+    # Precompute the encoder output and reuse it
+    # for every token we get from decoder
+    encoder_output = model.encode(source, source_mask)
+
+    # Initialize the decoder input with SOS token
+    decoder_input = torch.empty(1, 1).fill_(sos_id).type_as(source).to(device)
+    while True:
+        if decoder_input.size(1) > max_len:
+            break
+
+        # Build mask for the decoder input
+        decoder_mask = causal_mask(
+            decoder_input.size(1)).type_as(source_mask).to(device)
+        
+        # Calculate output of decoder
+        out = model.decode(
+            encoder_output, source_mask, decoder_input, decoder_mask
+        )
+
+        # Get next token
+        prob = model.project(out[:, -1])
+        # Select the token with the highest probability
+        _, next_word = torch.max(prob, dim=1)
+        decoder_input = torch.cat([
+            decoder_input,
+            torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)
+        ], dim=1)
+
+        if next_word == eos_id:
+            break
+    
+    return decoder_input.squeeze(0)
 
 
 if __name__ == '__main__':
-
     processing_dir = '/opt/ml/processing'
 
     # Create config
@@ -47,4 +88,41 @@ if __name__ == '__main__':
     model.load_state_dict(model_checkpoint['model_state_dict'])
     model.eval()
 
+    count = 0
+    source_texts = []
+    expected = []
+    predicted = []
+
+    with torch.no_grad():
+        for batch in valid_dataloader:
+            count += 1
+            encoder_input = batch['encoder_input'].to(device)
+            encoder_mask = batch['encoder_mask'].to(device)
+
+            assert encoder_input.size(0) == 1, 'Batch size must be 1'
+
+            model_output = greedy_decode(
+                model, encoder_input, encoder_mask, tokenizer_src,
+                tokenizer_tgt, config['seq_len'], device
+            )
+
+            source_txt = batch['src_text'][0]
+            target_txt = batch['tgt_text'][0]
+
+            model_out_txt = tokenizer_tgt.decode(
+                model_output.detach().cpu().numpy()
+            )
+
+            source_texts.append(source_txt)
+            expected.append(target_txt)
+            predicted.append(model_out_txt)
+
+            print(f'SOURCE: {source_txt}')
+            print(f'TARGET: {target_txt}')
+            print(f'PREDICTED: {model_out_txt}')
+    
+    # Compute BLEU score
+    metric = BLEUScore()
+    bleu_score = metric(predicted, expected)
+    print(f'BLEU score: {bleu_score.numpy().tolist():.3f}')
     
