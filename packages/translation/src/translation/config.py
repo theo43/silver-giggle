4ca4a5523a3bc887ee1b3d30fc7e7aa8b4da3dbf
theo43 <27286826+theo43@users.noreply.github.com>
2024-04-12 14:17:14 +0200

    feat(computes): 1 percent of train data

--- packages/translation/src/translation/config.py
+++ packages/translation/src/translation/config.py
@@ -5,7 +5,7 @@ def get_config():
     return {
         'batch_size': 8,
         'num_epochs': 3,
-        'download_size': 2,
+        'download_size': 1,
         'lr': 10**-4,
         'seq_len': 350,
         'd_model': 512,
