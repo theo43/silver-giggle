6b40ca78d288875d547a870a03f033fef541e87d
theo43 <27286826+theo43@users.noreply.github.com>
2024-05-27 14:43:22 +0200

    feat(train_gpu): adapt main.py app file and Dockerfile

--- production/main.py
+++ production/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pathlib import Path
 import torch
+import uvicorn
 from translation.config import get_config
 from translation.model import get_model
 
@@ -11,31 +12,36 @@ BASE_PATH = Path(__file__).resolve().parent
 MODEL_PATH = BASE_PATH / f'models/weights/tmodel_{num_epochs-1:02d}.pt'
 
 
-if __name__ == '__main__':
-    app = FastAPI(
-        title='Translation Model API',
-        description='API for machine translation',
-    )
-    # Load the tokenizers and model
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    checkpoint = torch.load(str(MODEL_PATH), map_location=device)
-    tokenizer_src = checkpoint['tokenizer_src']
-    tokenizer_tgt = checkpoint['tokenizer_tgt']
-    model = get_model(
+# Create app
+app = FastAPI(
+    title='Translation Model API',
+    description='API for machine translation',
+)
+# Load the tokenizers and model
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+checkpoint = torch.load(str(MODEL_PATH), map_location=device)
+tokenizer_src = checkpoint['tokenizer_src']
+tokenizer_tgt = checkpoint['tokenizer_tgt']
+model = get_model(
+    config,
+    tokenizer_src.get_vocab_size(),
+    tokenizer_tgt.get_vocab_size()
+).to(device)
+model.load_state_dict(checkpoint['model_state_dict'])
+model.eval()
+
+
+@app.get("/")
+def predict(text: str):
+    result = model.translate(
+        text,
         config,
-        tokenizer_src.get_vocab_size(),
-        tokenizer_tgt.get_vocab_size()
-    ).to(device)
-    model.load_state_dict(checkpoint['model_state_dict'])
-    model.eval()
+        tokenizer_src,
+        tokenizer_tgt,
+        device
+    )
+    return {"translation": result}
 
 
-    @app.get("/")
-    def predict(text: str):
-        translation = model.translate(
-            text, config,
-            tokenizer_src,
-            tokenizer_tgt,
-            device
-        )
-        return {"translation": translation}
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0,', port='8000')
